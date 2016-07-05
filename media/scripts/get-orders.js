c3cbd5a213be37c63e6900c774b0ac14d4e8c93a
Joshua Myerson <myersonjosh1@gmail.com>
2016-07-05 13:36:53 -0400

    Removed Google Fonts and Roboto Font

--- media/scripts/get-orders.js
+++ media/scripts/get-orders.js
@@ -1,30 +1,25 @@
 // Process the order on client sidebar
 
-var socket = io.connect();
-console.log(socket);
+//var socket = io.connect();
+//console.log(socket);
 var order = [];
 /* Order Array Map */
 /* 0 - OrderID, 1 - Student ID, 2 - Student Name, 3 - entrees, 4 - fruitsVegetablesArray[0], 5 - fruitsVegetablesArray[1] or null, 6 - beverages */
 
 // When the order is confirmed, create the disabled cookie
-$(document).ready(function () {
+console.log("AMERICA WAS CALLED");
+	// Request orders from DB
+	//socket.emit('GetOrders', SchoolID);
 	
-	function getOrders(SchoolID) {
+	// When server returns orders, display
+	/*socket.on('GetOrders', function(SchoolID) {
+		// Set Order Selections
+		order[0] = entrees;
+		order[1] = fruitsVegetablesArray[0];
+		if (fruitsVegetablesArray[1] != null) {
+			order[2] = fruitsVegetablesArray[1];
+		}
+		order[3] =  beverages;
 		
-		// Request orders from DB
-		socket.emit('GetOrders', SchoolID);
-		
-		// When server returns orders, display
-		socket.on('GetOrders', function(SchoolID) {
-			// Set Order Selections
-			order[0] = entrees;
-			order[1] = fruitsVegetablesArray[0];
-			if (fruitsVegetablesArray[1] != null) {
-				order[2] = fruitsVegetablesArray[1];
-			}
-			order[3] =  beverages;
-			
-			console.log(order);
-		});
-	}
-});
+		console.log(order);
+	});*/
